4ffe2489227216941adf9ca042da653071a0796c
initdb <benedict.schwind@live.de>
2019-03-20 20:08:20 +0100

    add number 2 of introduction exercise.

---
+++ 01-einfuehrung/02-serial/serial_com.ino
@@ -0,0 +1,25 @@
+String input;
+
+void setup() {
+    Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+    // empty
+}
+
+void serialEvent() {
+    // check if something is there
+    if (Serial.available() > 0) {
+        // consume data regardless of input
+        input = Serial.readString();
+
+        if (input.length() == 4) {
+            Serial.print("valid input: ");
+            Serial.println(input);
+        } else {
+            Serial.print("discarded: ");
+            Serial.println(input);
+        }
+    }
+}
